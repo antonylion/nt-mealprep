daf2099fbedd75e2e5c2be11297ec74d4fc42db8
Antony Zappacosta <antony.zappacosta@studio.unibo.it>
2024-07-02 10:15:09 +0200

    vercel DB configuration

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,23 +1,35 @@
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
-import mysql.connector
+import psycopg2
 from config import db_config
+from dotenv import load_dotenv
+import os
 import json
 import pandas
+
+load_dotenv()  # This will load environment variables from the .env file
 
 app = Flask(__name__)
 CORS(app)
 
 # Create a function to connect to the database
 def connect_to_database():
-    return mysql.connector.connect(**db_config)
+    return psycopg2.connect(**db_config)
 
 @app.route('/')
 def populate_planner():
     conn = connect_to_database()
     cursor = conn.cursor()
-    cursor.execute("SELECT RECIPES.name, INGREDIENTS.name, HASQ.quantity from RECIPES, INGREDIENTS, HASQ where RECIPES.name=HASQ.name_recipe and INGREDIENTS.name=HASQ.name_ingredient ORDER BY RECIPES.name")
+    cursor.execute("""
+        SELECT RECIPES.name, INGREDIENTS.name, HASQ.quantity
+        FROM RECIPES
+        JOIN HASQ ON RECIPES.name = HASQ.name_recipe
+        JOIN INGREDIENTS ON INGREDIENTS.name = HASQ.name_ingredient
+        ORDER BY RECIPES.name
+    """)
     data = cursor.fetchall()
+    cursor.close()
+    conn.close()
     return jsonify(data)
 
 def mock_planner():
