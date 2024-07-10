44ed8c60c3027ca46af3165472d31b0ad650ea8d
Antony Zappacosta <antony.zappacosta@studio.unibo.it>
2024-07-10 11:24:07 +0200

    Trial 8

--- try_secrets.py
+++ try_secrets.py
@@ -1,7 +1,7 @@
 import os
 
 HOST = os.environ['POSTGRES_HOST']
-DATABASE = os.environ['POSTGRES_DATABASE']
+#DATABASE = os.environ['POSTGRES_DATABASE']
 
 print(f"The secret is {HOST}")
-print(f"The secret is {DATABASE}")
+#print(f"The secret is {DATABASE}")
