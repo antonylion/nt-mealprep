db665bbe8cab3bd5d28a6d1e74314c7f9b25a193
Antony Zappacosta <antony.zappacosta@studio.unibo.it>
2024-07-10 11:11:49 +0200

    Trial 4

--- flask-server/config.py
+++ flask-server/config.py
@@ -1,9 +1,9 @@
 import os
 
 db_config = {
-    'host': os.getenv('POSTGRES_HOST'),
-    'database': os.getenv('POSTGRES_DATABASE'),
-    'user': os.getenv('POSTGRES_USER'),
-    'password': os.getenv('POSTGRES_PASSWORD'),
+    'host': os.environ['POSTGRES_HOST'],
+    'database': os.environ['POSTGRES_DATABASE'],
+    'user': os.environ['POSTGRES_USER'],
+    'password': os.environ['POSTGRES_PASSWORD'],
     'port': 5432  # Default port for PostgreSQL is 5432
 }
---
+++ try_secrets.py
@@ -0,0 +1,5 @@
+import os
+
+HOST = os.environ['POSTGRES_HOST']
+
+print(f"The secret is {HOST}")
